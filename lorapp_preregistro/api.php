df92eee6f8567e3ead0656ece1d52acca96757f9
camilogarzon <camilo.garzon@secuencia24.com>
2015-04-23 08:25:37 -0500

    se sube pagina de preregistro y cambio en CSS de lorapp_alfa

---
+++ lorapp_preregistro/api.php
@@ -0,0 +1,46 @@
+<?php
+
+session_start();
+/**
+ * en este archivo se atienden todas las peticiones AJAX
+ */
+$rqst = $_REQUEST;
+$op = isset($rqst['op']) ? $rqst['op'] : '';
+header("Content-type: application/javascript; charset=utf-8");
+header("Cache-Control: max-age=15, must-revalidate");
+header('Access-Control-Allow-Origin: *');
+
+include 'classes/DbConection.php';
+include 'classes/Util.php';
+
+switch ($op) {
+    case 'getnote':
+        include 'classes/Note.php';
+        echo json_encode(Note::getAll($rqst));
+        break;
+
+    case 'savenote':
+        include 'classes/Note.php';
+        echo json_encode(Note::save($rqst));
+        break;
+
+    case 'register':
+        include 'classes/Note.php';
+        echo json_encode(Note::register($rqst));
+        break;
+
+    case 'preregistro':
+        include 'classes/Note.php';
+        echo json_encode(Note::preregistro($rqst));
+        break;
+
+    case 'deletenote':
+        include 'classes/Note.php';
+        echo json_encode(Note::delete($rqst));
+        break;
+
+    default:
+        echo 'OPERACION NO DISPONIBLE';
+        break;
+}
+?>
